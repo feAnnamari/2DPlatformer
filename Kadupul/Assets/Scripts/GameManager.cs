27eb8e68aa2c4ff9991d2e33cd71af771984f8e0
Annamária Fekete <fe.annamari@gmail.com>
2019-01-02 23:18:05 +0100

    Final

---
+++ Kadupul/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance;
+	[SerializeField] private GameObject _textPoint;
+	[SerializeField] private GameObject _endText;
+
+	private int prevPoint;
+
+	private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+		_endText.GetComponent<TextMeshProUGUI>().text = "0 of 5 crystals have been collected";
+    }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddPoint(int point)
+	{
+		string text = (prevPoint+point).ToString();
+		_textPoint.GetComponent<TextMeshProUGUI>().text = text;
+		_endText.GetComponent<TextMeshProUGUI>().text = text + " of 5 crystals have been collected";
+		prevPoint = prevPoint+point;
+		Debug.Log(text);
+	}
+}
